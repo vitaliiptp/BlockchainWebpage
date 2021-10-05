d7f4526d009cfd4c814b1a3231a46781f675a952
vitaliiptp <vitalii.potapenko@gmail.com>
2021-10-05 22:34:40 +0200

    back-up final version of branch. Defi is done

--- script.js
+++ script.js
@@ -39,3 +39,28 @@ function showSlides(n) {
 }
 
 // ___________________________________________Carousel-End___________________________________________
+
+
+// ___________________________________________Hamburger-Start___________________________________________
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}
+
+
+// ___________________________________________Hamburger-End___________________________________________
