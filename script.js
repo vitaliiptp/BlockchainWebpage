8ed4d75c55a4cd8d52b146e0f2b5dcbaf9ca0cd0
vitaliiptp <vitalii.potapenko@gmail.com>
2021-10-05 22:09:54 +0200

    updated footer

--- script.js
+++ script.js
@@ -1,41 +1,41 @@
+const email = document.querySelector(".fa-envelopeonclick");
 
+email.onclick = () => {
+  Swal.fire("Oops...", "Looks like the backend is not connected yet!", "error");
+  // alert("Ooops, looks like the backend is not connected yet.");
+};
 
 // ___________________________________________Carousel-Start___________________________________________
-var slideIndex = 1;
+let slideIndex = 1;
 showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
-    showSlides(slideIndex += n);
+  showSlides((slideIndex += n));
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-    showSlides(slideIndex = n);
+  showSlides((slideIndex = n));
 }
 
 function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
+  const slides = document.getElementsByClassName("mySlides");
+  const dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
 
 // ___________________________________________Carousel-End___________________________________________
-
-const email = document.querySelector(".fa-envelopeonclick");
-
-
-email.onclick = () => {
-    Swal.fire('Oops...', 'Looks like the backend is not connected yet!', 'error');
-    // alert("Ooops, looks like the backend is not connected yet.");
-};
