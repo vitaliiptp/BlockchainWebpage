15ca4d908d3f5a92d6f61d56aeb4ba91dbc54bbe
vitaliiptp <vitalii.potapenko@gmail.com>
2021-10-06 16:17:22 +0200

    restructured js into diff files

--- script.js
+++ script.js
@@ -6,64 +6,8 @@ email.onclick = () => {
 };
 
 
-
-
-// ___________________________________________Carousel-Start___________________________________________
-let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides((slideIndex += n));
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
-
-function showSlides(n) {
-  const slides = document.getElementsByClassName("mySlides");
-  const dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (let i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-}
-
-// ___________________________________________Carousel-End___________________________________________
-
-
-// ___________________________________________Hamburger-Start___________________________________________
-
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
-
-hamburger.addEventListener("click", mobileMenu);
-
-function mobileMenu() {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
-}
-
-const navLink = document.querySelectorAll(".nav-link");
-
-navLink.forEach(n => n.addEventListener("click", closeMenu));
-
-function closeMenu() {
-  hamburger.classList.remove("active");
-  navMenu.classList.remove("active");
-}
-
-
-// ___________________________________________Hamburger-End___________________________________________
+const contsctSubmit = document.querySelector(".submit-button");
+contsctSubmit.addEventListener('click', (event) => {
+  Swal.fire("Oops...", "Looks like the backend is not connected yet!", "error");
+  // alert("Ooops, looks like the backend is not connected yet.");
+});
