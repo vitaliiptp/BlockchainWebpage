e7caca7b6045b16ea73a09e2270efeab8194e74a
vitaliiptp <vitalii.potapenko@gmail.com>
2021-10-06 22:26:05 +0200

    started working on responsive design

--- hamburger.js
+++ hamburger.js
@@ -1,23 +1,23 @@
-// // ___________________________________________Hamburger-Start___________________________________________
-//
-// const hamburger = document.querySelector(".hamburger");
-// const navMenu = document.querySelector(".nav-menu");
-//
-// hamburger.addEventListener("click", mobileMenu);
-//
-// function mobileMenu() {
-//   hamburger.classList.toggle("active");
-//   navMenu.classList.toggle("active");
-// }
-//
+// ___________________________________________Hamburger-Start___________________________________________
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".menu-bar");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
 // const navLink = document.querySelectorAll(".nav-link");
 //
 // navLink.forEach(n => n.addEventListener("click", closeMenu));
 //
 // function closeMenu() {
-//   hamburger.classList.remove("active");
-//   navMenu.classList.remove("active");
+//     hamburger.classList.remove("active");
+//     navMenu.classList.remove("active");
 // }
-//
-//
-// // ___________________________________________Hamburger-End___________________________________________
+
+
+// ___________________________________________Hamburger-End___________________________________________
