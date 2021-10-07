bc315f1831d202cdf9f728bc0f920aac249b0491
Ugo <ugotheallet@gmail.com>
2021-10-07 16:29:14 +0200

    added dark theme

---
+++ dark.js
@@ -0,0 +1,43 @@
+// check for saved 'darkMode' in localStorage
+let darkMode = localStorage.getItem('darkMode'); 
+
+const darkModeToggle = document.querySelector('#chk');
+
+const enableDarkMode = () => {
+  // 1. Add the class to the body
+  document.body.classList.add('darkmode');
+  // 2. Update darkMode in localStorage
+  localStorage.setItem('darkMode', 'enabled');
+  
+  
+}
+
+
+const disableDarkMode = () => {
+  // 1. Remove the class from the body
+  document.body.classList.remove('darkmode');
+  // 2. Update darkMode in localStorage 
+  localStorage.setItem('darkMode', null);
+  
+}
+ 
+// If we already visited and enabled the DarkMode, it will start enabled.
+
+if (darkMode === 'enabled') {
+    darkModeToggle.checked = true /*This keep the button at the same place if it's dark. */
+   enableDarkMode();
+}
+
+// When we click on the button :
+darkModeToggle.addEventListener('click', () => {
+  // get the darkMode setting (in localStorage)
+  darkMode = localStorage.getItem('darkMode'); 
+  
+  // if it is not currently enabled, enable it!
+  if (darkMode !== 'enabled') {
+    enableDarkMode();
+  // if it has been enabled, turn it off ! 
+  } else {  
+    disableDarkMode(); 
+  }
+});
